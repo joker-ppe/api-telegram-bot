d77265032e8b9b6fdb96a8571a17d7195ec093b5
Joker pp_e <voix.momotalk@gmail.com>
2024-02-19 11:51:39 +0700

    Update report.service.ts

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -960,7 +960,7 @@ export class ReportService implements OnModuleInit {
       endDate,
     );
 
-    const dataTet = await this.getListUsersWithDataCron('2024-02-05', endDate);
+    // const dataTet = await this.getListUsersWithDataCron('2024-02-05', endDate);
 
     await this.prismaService.data.update({
       where: {
@@ -969,7 +969,7 @@ export class ReportService implements OnModuleInit {
       data: {
         adminDataToDay: JSON.stringify(dataToDay),
         adminDataThisWeek: JSON.stringify(dataThisWeek),
-        adminDataTet: JSON.stringify(dataTet),
+        adminDataTet: '',
       },
     });
 
