fd3de30138b38683c9b41d0c972ba14c31983bb1
Joker pp_e <voix.momotalk@gmail.com>
2024-03-17 17:56:55 +0700

    Update report.service.ts

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -820,8 +820,8 @@ export class ReportService implements OnModuleInit {
     textReport += `Tổng toang toàn sàn: ${totalLost.toLocaleString()}\n`;
     textReport += `=> ${(totalAmount - totalLost).toLocaleString()}`;
 
-    console.log(textReport);
-    // await this.sendMessage(textReport);
+    // console.log(textReport);
+    await this.sendMessage(textReport);
   }
 
   async calculatorResult(betFullData: BetItem[]) {
